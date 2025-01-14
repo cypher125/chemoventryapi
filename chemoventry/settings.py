d9f5205e6f5a3c20299ec39e2bb0efb6e33323a8
Osawaye Cyrus <80757178+cypher125@users.noreply.github.com>
2025-01-15 00:10:47 +0100

    Update settings.py
    added more host to the cors

--- chemoventry/settings.py
+++ chemoventry/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
     ".vercel.app",
     ".github.dev",
     "localhost",
+    "127.0.0.1",
 
 ]
 
@@ -109,6 +110,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 """
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
+    "http://127.0.0.1:3000",
     "http://localhost:3000",
     "http://localhost:6789",
 ] 
