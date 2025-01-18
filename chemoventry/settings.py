2203af6a3a05186a3b879e571c7434d0ac5515de
Osawaye Cyrus <80757178+cypher125@users.noreply.github.com>
2025-01-18 03:08:48 +0000

    Update settings.py

--- chemoventry/settings.py
+++ chemoventry/settings.py
@@ -113,6 +113,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
     "http://localhost:6789",
+    "https://chemoventry.vercel.app",
 ] 
 
 CORS_ALLOW_HEADERS = [
